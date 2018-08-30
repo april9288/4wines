361fa7cfe985f01c4f1243b304d7199bdb51f092
Jongho Kim <april9288@gmail.com>
2018-08-30 16:21:47 -0700

    add button

--- src/Components/FirstPG.js
+++ src/Components/FirstPG.js
@@ -10,11 +10,16 @@ const FirstPG = () => {
             </div>
             <div className="container first_page_container">
                 <div className="content-center brand">
-                    <h1 className="logo animated fadeInDownBig">4WINES</h1>
-                    <h3 className="logo_sub animated fadeInDownBig">A greate way to start exploring new wines</h3>
-                    <button className="btn btn-info btn-lg">Explore now</button>
+                    <h1 className="logo animated fadeInUpBig">4WINES</h1>
+                    <h3 className="logo_sub animated fadeInUpBig">A greate way to start exploring new wines</h3>
+                    <button className="btn btn-info btn-lg try_demo">Try our demo<span style={{marginRight:"1rem"}}></span><i className="now-ui-icons arrows-1_minimal-right animated infinite slideInLeft"></i><i className="now-ui-icons arrows-1_minimal-right animated infinite slideInLeft"></i></button>
+                   
+                    <div>
+                    <button className="btn btn-link explore_more">Explore more<span style={{marginRight:"1rem"}}></span><i className="now-ui-icons arrows-1_minimal-down"></i></button>
+                    </div>
                    {/*<Quiz />*/}
                 </div>
+                
             </div>
         </div>
 
