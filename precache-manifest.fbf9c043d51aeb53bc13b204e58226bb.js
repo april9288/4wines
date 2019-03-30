2a21e6ae4c04c0e1456f794b0e9c433bf48f124d
Jongho Kim <april9288@gmail.com>
2019-03-29 22:22:49 -0700

    Updates

---
+++ precache-manifest.fbf9c043d51aeb53bc13b204e58226bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd904c385e04757ae030",
+    "url": "/4wines/static/js/runtime~main.670dda92.js"
+  },
+  {
+    "revision": "8ccf67a3e4d5a88d34f9",
+    "url": "/4wines/static/js/main.a16c5f92.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/4wines/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8ccf67a3e4d5a88d34f9",
+    "url": "/4wines/static/css/main.4f5a346d.chunk.css"
+  },
+  {
+    "revision": "0a4f21d23372c98c44337f5fa2b88b3c",
+    "url": "/4wines/index.html"
+  }
+];
